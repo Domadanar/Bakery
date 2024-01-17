d3d30612a5b55a6c0ec601738372e25350dba822
Domadanar <denik8223@gmail.com>
2024-01-17 14:41:48 +0200

    fix button

--- js/modal-mobile.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-mobile-btn-open');
-const modalBtnClose = document.querySelector('.modal-mobile-btn-close');
-
-const toggleModal = () => modal.classList.toggle('is-hidden');
-
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
